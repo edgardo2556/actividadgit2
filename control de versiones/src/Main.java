636b27c822674df747f9207ea446fff464dbbfb7
edgardo2556 <jugador.eo@gmail.com>
2024-12-16 16:10:33 +0100

    MATRIZGENERADA

--- control de versiones/src/Main.java
+++ control de versiones/src/Main.java
@@ -5,5 +5,23 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-       
+     Scanner scanner = new Scanner(System.in);
+     Random ran = new Random();
+     int [][] array = new int[5][5] ;
+     for (int i = 0 ; i < 5 ; i++){
+         for (int j =0 ; j< 5 ; j++ ){
+             array[i][j] = ran.nextInt(101);
+
+         }
+     }
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+
+
+    }
 }
