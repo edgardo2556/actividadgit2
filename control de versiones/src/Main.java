845eed1f468bf37c2a808d5f7ddce9279bf990cf
edgardo2556 <jugador.eo@gmail.com>
2024-12-16 16:26:11 +0100

    BUSQUEDA DE NUMERO

--- control de versiones/src/Main.java
+++ control de versiones/src/Main.java
@@ -20,8 +20,22 @@ public class Main {
             }
             System.out.println();
         }
-        
-
+        System.out.println("ingrese el numero para buscar :");
+        int numeroParaBuscar = scanner.nextInt();
+        boolean numeroEncontrado  = false ;
+        for (int i = 0 ; i < 5 ; i++){
+            for (int j =0 ; j< 5 ; j++ ){
+                if (array[i][j] == numeroParaBuscar){
+                    System.out.println("EL numero esta en la fila " + i + " " + "columna " + j);
+                    numeroEncontrado = true ;
+                }
+            }
+        }
+        if (!numeroEncontrado){
+            System.out.println("EL numero no esta en la matriz") ;
+        }else {
+            System.out.println("El numero esta en la matriz") ;
+        }
 
     }
 }
